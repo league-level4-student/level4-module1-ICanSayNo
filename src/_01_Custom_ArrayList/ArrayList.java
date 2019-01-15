c6a31844f5dac70380fbc9d48ba99ddfe8e238cb
ICanSayNo <league@WTS20.attlocal.net>
2019-01-14 19:58:12 -0800

    jeff

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -12,28 +12,74 @@ public class ArrayList <T>{
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		System.out.println(list);
+		return list[loc];
 	}
 	
 	public void add(T val) {
-		
+		T[] newList = (T[]) new Object [list.length+1];
+		for (int i = 0; i < list.length; i++) {
+			newList[i] = list[i];
+		}
+		newList[newList.length-1] = val;
+		for (int i = 0; i < newList.length; i++) {
+			System.out.println(newList[i]);
+		}
+		list = newList;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] newList = (T[]) new Object [list.length+1];
+		boolean inserted = false;
+		for (int i = 0; i < list.length; i++) {
+			if (i == loc) {
+				newList[loc] = val;
+				inserted = true;
+			}
+			if (!inserted) {
+				newList[i] = list[i];
+			}
+			if (inserted) {
+				newList[i+1] = list[i];
+			}
+		}
+		list = newList;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		list[loc]=val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		T[] newList = (T[]) new Object [list.length-1];
+		boolean removed = false;
+		for (int i = 0; i < list.length-1; i++) {
+			if (i == loc) {;
+				removed = true;
+			}
+			if (!removed) {
+				newList[i] = list[i];
+			}
+			if (removed) {
+				newList[i] = list[i+1];
+			}
+		}
+		list = newList;
+
 	}
 	
 	public boolean contains(T val) {
+		boolean contains = false;
+		for (int i = 0; i < list.length; i++) {
+			if (list[i]==val){
+				contains = true;
+			}
+		}
+		return contains;
+	}
+
+	public int size() {
 		
-		return false;
+		return list.length;
 	}
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class CustomButtonOptionPanes {
 	public static void main(String[] args) {
-		String[] options = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		String[] options = {  "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday", "Sunday"  };
 
 		int input = JOptionPane.showOptionDialog(null, "Choose a day of the week", "Custom Buttons", 0, -1, null,
 				options, 0);
@@ -13,5 +13,29 @@ public class CustomButtonOptionPanes {
 		
 		//use a switch statement to do something cool for each option
 		
+		switch(input) {
+		case 0: 
+			System.out.println("0");
+			System.out.println("oof");
+			break;
+		case 1: 
+			System.out.println("1");
+			break;
+		case 2: 
+			System.out.println("2");
+			break;
+		case 3: 
+			System.out.println("3");
+			break;
+		case 4: 
+			System.out.println("4");
+			break;
+		case 5: 
+			System.out.println("5");
+			break;
+		case 6: 
+			System.out.println("6");
+			break;
+		}
 	}
 }
